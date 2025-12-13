078f6a1690d680d16c2542cd412aa7d40f573c6c
Sanders <tmdrl5099@gmail.com>
2025-12-13 21:32:28 +0900

    JSP 2일차

---
+++ chapter03_servlet/src/main/java/org/joonzis/ex/Ex06_request.java
@@ -0,0 +1,52 @@
+package org.joonzis.ex;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Ex06_request")
+public class Ex06_request extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public Ex06_request() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String name = request.getParameter("name");
+		int kor = Integer.parseInt(request.getParameter("kor"));
+		String eng = request.getParameter("eng");
+		String mat = request.getParameter("mat");
+		double avg = 	(kor + 
+						Integer.parseInt(eng) + 
+						Integer.parseInt(mat) ) / 3.0;
+		char grade;
+		if(avg >= 90) grade = 'A';
+		else if(avg >= 80) grade = 'B';
+		else if(avg >= 70) grade = 'C';
+		else if(avg >= 60) grade = 'D';
+		else grade = 'F';
+		
+		System.out.println("이름 : " + name);
+		System.out.println("평균 : " + avg);
+		System.out.println("학점 : " + grade);
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
+
+
+
+
+
+
+
+
